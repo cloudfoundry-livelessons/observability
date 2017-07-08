1780ebb4fd5d63a0a8c77b9f0307f418bdcac682
Josh Long <josh@joshlong.com>
2017-07-08 01:57:42 -0700

    adding an initial RabbitMQ queue-depth based autoscaler. currently the implementation only scales the instance count up. it could just scale the count back to 1 and let it autoscale up again as the average stabalizes.

---
+++ rabbit-queue-autoscaler/src/test/java/com/example/rabbitqueueautoscaler/RabbitQueueAutoscalerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.rabbitqueueautoscaler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitQueueAutoscalerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
