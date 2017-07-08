a5985c6b27be7f446172ba8c2e3792779ceab968
Josh Long <josh@joshlong.com>
2017-07-08 09:24:51 -0700

    added deploy.sh script

---
+++ autoscaler/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+find . -iname pom.xml |  xargs -Ipom mvn -f pom clean install
+
+cf cs cloudamqp lemur autoscaler-rabbit
+
+# cd consumer
+# cf push
+# cd ..
+#
+# cd producer
+# cf push
+# cd ..
+
+cd rabbit-queue-autoscaler
+cf push --no-start
+
+# autoscaler vars
+cf set-env rabbit-queue-autoscaler autoscaler_rabbitMq_applicationName hipy
+cf set-env rabbit-queue-autoscaler autoscaler_rabbitMq_queueName autoscaler-demo
+
+# CF vars
+cf set-env rabbit-queue-autoscaler CF_API $CF_API
+cf set-env rabbit-queue-autoscaler CF_ORG $CF_ORG
+cf set-env rabbit-queue-autoscaler CF_PASSWORD $CF_PASSWORD
+cf set-env rabbit-queue-autoscaler CF_SPACE $CF_SPACE
+cf set-env rabbit-queue-autoscaler CF_USER $CF_USER
+
+cf start rabbit-queue-autoscaler
+cd ..
