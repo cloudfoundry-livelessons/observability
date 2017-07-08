343e341a3c59645eb24e818441e3c3b83fb45f81
Josh Long <josh@joshlong.com>
2017-07-08 02:20:35 -0700

    adding the producer and consumer for the autoscaler demo.

--- autoscaler/consumer/src/main/java/demo/ConsumerApplication.java
+++ autoscaler/consumer/src/main/java/demo/ConsumerApplication.java
@@ -1,7 +1,6 @@
 package demo;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -10,31 +9,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 
+@Slf4j
 @EnableBinding(Sink.class)
 @SpringBootApplication
 public class ConsumerApplication {
 
- @Bean
- IntegrationFlow incoming(Sink channels) {
-  Log log = LogFactory.getLog(getClass());
-  return IntegrationFlows
-   .from(channels.input())
-   .handle(
-    message -> {
-     try {
-      long duration = 2000L;
-      log.info("sleeping " + duration
-       + "ms to simulate long-running processing");
-      Thread.sleep(duration);
-     }
-     catch (InterruptedException e) {
-      throw new RuntimeException(e);
-     }
-     log.info("new message! " + message.getPayload());
-    }).get();
- }
+    @Bean
+    IntegrationFlow incoming(Sink channels) {
+        return IntegrationFlows
+                .from(channels.input())
+                .handle(message -> {
+                    try {
+                        long duration = 2000L;
+                        Thread.sleep(duration);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    log.info("new message! " + message.getPayload());
+                }).get();
+    }
 
- public static void main(String args[]) {
-  SpringApplication.run(ConsumerApplication.class, args);
- }
+    public static void main(String args[]) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
 }
